707e6bf1e8732004f24b9e38300de466d031f93e
rute-sophie <rute.c.lemos@gmail.com>
2025-08-14 21:57:55 +0100

    completed option 3 exercise

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -7,8 +7,8 @@ struct Point {
 fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
-    // TODO: Fix the compiler error by adding something to this match statement.
-    match optional_point {
+    // TODO: Fix the compiler error by adding something to this match statement - done, added borrowed value.
+    match &optional_point {
         Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
